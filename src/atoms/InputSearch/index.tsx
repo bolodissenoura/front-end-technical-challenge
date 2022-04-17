d46ae3cf01350b611d1656a7144f804e00ecab28
bolodissenoura <suamaemedeu@outlook.com>
2022-04-17 00:44:47 -0300

    :zap: Starting to add the funcionality of search input

--- src/atoms/InputSearch/index.tsx
+++ src/atoms/InputSearch/index.tsx
@@ -1,9 +1,19 @@
 import { TextField } from '@mui/material';
+import React, { useEffect, useState } from "react";
 import * as S from './styles'
 
 const InputSeach = () => {
+
+    const [search, setSearch] = useState('');
+
     return <S.IptSearch>
-        <TextField id="outlined-basic" label="Pesquisa" variant="outlined" />
+        {search}
+        <TextField
+            id="outlined-basic"
+            label="Pesquisa"
+            variant="outlined"
+            onChange={e => setSearch(e.target.value)}
+        />
     </S.IptSearch>;
 };
 
