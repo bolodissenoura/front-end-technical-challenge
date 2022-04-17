b34b344014f1058f85042074f78ca3865fa81926
bolodissenoura <suamaemedeu@outlook.com>
2022-04-17 00:16:21 -0300

    :zap: Add the api content

--- src/molecules/ListMap/index.tsx
+++ src/molecules/ListMap/index.tsx
@@ -1,17 +1,39 @@
 import { TextField, List, ListItem, ListItemAvatar, Avatar, ListItemText } from '@mui/material';
 import * as S from './styles'
+import React, { useEffect, useState } from "react";
+import api from '../../services/api'
 
 const ListMap = () => {
+    const [stores, setStores] = useState<any[]>([]);
+
+    useEffect(() => {
+        api
+            .get("/")
+            .then((response) => {
+                setStores(response.data.stores);
+            })
+            .catch((err) => {
+                console.error("ops! ocorreu um erro" + err);
+            });
+    }, []);
+
     return <S.ListSection>
         <List className="lt-principal">
-            <ListItem className="lt-item">
-                <ListItemAvatar>
-                    <Avatar>
-                        <h1>SP</h1>
-                    </Avatar>
-                </ListItemAvatar>
-                <ListItemText primary="Alameda Santos" secondary="29854.12" />
-            </ListItem>
+            {stores?.map((store, key) => {
+                return (
+                    <>
+                        <ListItem className="lt-item">
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <h1>SP</h1>
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary={store.name} secondary={store.name} />
+                        </ListItem>
+                    </>
+                )
+            })}
+
         </List>
     </S.ListSection>;
 };
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -7,8 +7,7 @@ export default function App() {
   return (
     <div className="App">
       <Frontend>
-        Lorem Ipsum
-        <p>hello my dear stalker</p>
+        
       </Frontend>
 
 
