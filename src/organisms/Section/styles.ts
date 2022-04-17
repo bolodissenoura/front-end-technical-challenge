d02cb06be1cf0c9ba07e990dad6f56c25fa4d9c5
bolodissenoura <suamaemedeu@outlook.com>
2022-04-16 23:30:27 -0300

    :lipstick: Styles section home component

---
+++ src/organisms/Section/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const SectionHome = styled.section`
+    justify-content:center;
+    align-items: center;
+    margin-left: auto;
+    margin-right: auto; 
+    margin-top: 50px;
+    background-color: #ffffff;
+    max-width: 80%;
+
+`
