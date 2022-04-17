d43052a96a9a072cabd9503dcd52582632d7b061
bolodissenoura <suamaemedeu@outlook.com>
2022-04-17 19:41:16 -0300

    :lipstick: Add some css to ListSection

--- src/molecules/ListMap/styles.ts
+++ src/molecules/ListMap/styles.ts
@@ -4,18 +4,30 @@ export const ListSection = styled.div`
     
     .lt-search{
         padding-bottom: 10px;
+        width: 400px;
     }
 
     .lt-principal{
-        background-color: gray;
+        background-color: white;
         height: 300px;
         min-width: 400px;
+        border-radius: 16px;
         overflow-y: scroll;
         overflow-x: hidden;
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
     }
     .lt-item{
         background-color: whitesmoke;
-        margin: 5px;
+        box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+        margin-bottom: 10px;
+        width: 300px;
         border-radius: 16px;
+        transition: transform 250ms;
+        margin-left: auto;
+        margin-right: auto;
+        :hover{
+            cursor: pointer !important;
+            transform: translateY(-5px);
+        }
     }
 `
