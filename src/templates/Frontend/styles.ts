7108aae286e37884a2ddf05094f65f71ada181c0
bolodissenoura <suamaemedeu@outlook.com>
2022-04-16 23:32:14 -0300

    :lipstick: add a provisory styles of home section

--- src/templates/Frontend/styles.ts
+++ src/templates/Frontend/styles.ts
@@ -1,21 +1,13 @@
 import styled from "styled-components";
 
-export const SectionHome = styled.section`
-    height: 100vh;
-    background-repeat: no-repeat; 
-    background-size: cover;
-    justify-content: center;
-    text-align: center;
+export const SectionHeader = styled.section`
+    height: auto;
 
 `
-export const SectionCard = styled.section`
+export const SectionMaps = styled.section`
     height: 100vh;
     background-repeat: no-repeat; 
     background-color: #313131;
     background-size: cover;
 
-`
-export const SectionAbout = styled.section`
-    height: auto;
-    background-color: #707070;
 `
